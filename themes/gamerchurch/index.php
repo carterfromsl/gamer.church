76021e79ddee83c094c002e6545d06b15813668f
Carter Lovelace <83387818+carterfromsl@users.noreply.github.com>
2025-03-07 11:40:29 -0600

    Create index.php

---
+++ themes/gamerchurch/index.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+get_template_part( 'header-full' );
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+get_template_part( 'entry' );
+comments_template();
+endwhile; endif;
+get_template_part( 'nav', 'below' );
+get_footer();
