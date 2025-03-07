503283a4c451fab7059794aab0099716b977150f
Carter Lovelace <83387818+carterfromsl@users.noreply.github.com>
2025-03-07 11:41:43 -0600

    Create shop-header.php

---
+++ themes/gamerchurch/shop-header.php
@@ -0,0 +1,5 @@
+<header id="shopHeader" role="banner">
+<nav id="shopMenu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+<?php wp_nav_menu( array( 'theme_location' => 'shop-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+</nav>
+</header>
