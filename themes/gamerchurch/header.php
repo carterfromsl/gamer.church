b802838960e41819c006da30a35a1ebeb4088366
Carter Lovelace <83387818+carterfromsl@users.noreply.github.com>
2025-03-07 11:40:15 -0600

    Create header.php

---
+++ themes/gamerchurch/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html class="gamerchurch" <?php language_attributes(); ?> <?php gamerchurch_schema_type(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width">
+<?php wp_head(); ?>
+</head>
+<body id="top" <?php body_class(); ?>>
+<div id="art">
+	<marquee width="100%" direction="up" scrollamount="3"><p>
+		<b>.</b><i>GAMER</i><b>.</b><i>CHURCH</i><b>.</b>
+		</p>
+	</marquee>
+</div>
+<?php wp_body_open(); ?>
